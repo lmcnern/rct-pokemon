6fd70739d5dfa1cebf7baea41e8af919a446d3c4
Lauren McNerney <lmcnern@outlook.com>
2015-08-21 19:00:56 -0700

    pokedex list

---
+++ js/components/Pokemon.jsx
@@ -0,0 +1,16 @@
+'use strict';
+
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <section>
+        <h2> {this.props.pokemon.name} </h2>
+        <p><strong>PokedexID</strong>: {this.props.pokemon.national_id}</p>
+        <p><strong>attack</strong>: {this.props.pokemon.attack}          <strong>defense</strong>: {this.props.pokemon.defense}</p>
+        <p><strong>defense</strong>: {this.props.pokemon.defense}        <strong>speed</strong>: {this.props.pokemon.speed}</p>
+    </section>
+    )
+  }
+});
